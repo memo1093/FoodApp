f31fc3f80d1e48ff1504c021158da1421a8df4f9
memo1093 <67466350+memo1093@users.noreply.github.com>
2020-11-26 13:56:54 +0300

    Design Fixes

--- foodapp.webui/wwwroot/js/site.js
+++ foodapp.webui/wwwroot/js/site.js
@@ -1,13 +1,24 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
+
 // Write your JavaScript code.
 var x = window.matchMedia("(max-width: 700px)");
 if (x.matches) {
     $("#hideOnSmall").hide(); 
 }
+$('.product-card').on('click',function(){$('.collapse').collapse('hide');})
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+      
 
 
 
 
+  
   
